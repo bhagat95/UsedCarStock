b108d19d2987331fda2ad7b0b4158677c230eee6
Shubham Bhagat <shubham.bhagat@aepl.com>
2017-06-27 17:26:19 +0530

    add api

---
+++ UsedCarStockAPI/Controllers/UsedCarController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using UsedCarStockAPI.Models;
+using Dapper;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace UsedCarStockAPI.Controllers
+{
+    //[Authorize]
+    public class UsedCarController : ApiController
+    {
+
+        //public string test()
+        //{
+        //    UsedCarModel usedCarModel = new UsedCarModel();
+
+        //    return usedCarModel.Year;
+        //}
+
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public UsedCarModel Get(int id)
+        {
+            Console.WriteLine(id + " GET " + usedCarModel);
+            return usedCarModel;
+        }
+
+        UsedCarModel usedCarModel;
+
+        // POST api/values
+        public UsedCarModel Post(UsedCarModel usedCarModel)
+        {
+            usedCarModel.Id = 99;
+
+            IDbConnection connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"]
+                .ConnectionString);
+
+            
+            var param = new DynamicParameters();
+
+            param.Add("price",usedCarModel.Price);
+            param.Add("year", usedCarModel.Year);
+            param.Add("kilometer", usedCarModel.Kilometer);
+            param.Add("fuel_type_id", usedCarModel.FuelType);
+            param.Add("city_id", usedCarModel.City);
+            param.Add("color_id", usedCarModel.Color);
+            param.Add("fuel_economy", usedCarModel.FuelEconomy);
+            
+
+            //using (var con = connection)
+            //{
+            //    int rowsInserted = con.Execute("[AddStock_AS]", param, commandType:CommandType.StoredProcedure);
+            //}
+
+
+            Console.WriteLine(usedCarModel + "");
+            return usedCarModel;
+        }
+
+        // PUT api/values/5
+        public UsedCarModel Put(int id, UsedCarModel usedCarModel)
+        {
+            Console.WriteLine(id+" "+usedCarModel);
+            this.usedCarModel = usedCarModel;
+            return usedCarModel;
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+
+    }
+}
---
+++ UsedCarStockAPI/Models/UsedCarModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace UsedCarStockAPI.Models
+{
+    public class UsedCarModel
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public int Year { get; set; }
+        public int Kilometer { get; set; }
+        public String FuelType { get; set; }
+        public String City { get; set; }
+        public String Color { get; set; }
+        public int FuelEconomy { get; set; }
+        public String Make { get; set; }
+        public String Model { get; set; }
+        public String Version { get; set; }
+        public String ImageURI { get; set; }
+        public bool IsAvailable { get; set; }
+        
+
+    }
+}
