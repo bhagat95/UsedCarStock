8ecfa15caa5298a9fdb5a16059e003e16550feab
Shubham Bhagat <shubham.bhagat@aepl.com>
2017-06-27 13:02:24 +0530

    ProjectStarted

---
+++ UsedCarStock/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UsedCarStock.Startup))]
+namespace UsedCarStock
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ UsedCarStockAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace UsedCarStockAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
